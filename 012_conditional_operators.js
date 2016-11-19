7726d73976d90aec8081b7a552ca09ce107fcf95
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-11-19 13:43:49 -0800

    Conditional Operators

---
+++ 012_conditional_operators.js
@@ -0,0 +1,23 @@
+var x = 100;
+if (x == 200) {
+    console.log('Condition passed');
+}
+if (x === 100) {
+    console.log('Condition passed');
+}
+if (x != 200) {
+    console.log('Condition passed!');
+}
+if (x > 39) {
+    console.log('Condition passed!');
+}
+if (x >= 50) {
+    console.log('Condition granted!');
+}
+if (x < 100) {
+    console.log('Condition passed');
+}
+if (x <= 400) {
+    console.log('Condition accepted!');
+}
+//# sourceMappingURL=012_conditional_operators.js.map
