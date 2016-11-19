a5cceef8c4fdda7db17eee0c55f70816febea173
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-11-18 16:15:44 -0800

    TypeScript Conditionals

---
+++ 011_conditionals.ts
@@ -0,0 +1,9 @@
+let password : string = 'zzzzzzz';
+
+if(password == 'abcabcabc') {
+  console.log('Yes , you are typing the right password! ');
+} else if(password == 'zzzzzzz') {
+  console.log('YES, zzzzzzz is the correct password.');
+} else {
+  console.log('Sorry, permission denied.');
+}
