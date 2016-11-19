a216f5cd2f2fc6a795502f3917765574cf9cc911
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-11-19 14:27:19 -0800

    Compound conditionals

---
+++ 013_compound_conditionals.ts
@@ -0,0 +1,18 @@
+let email : string = 'test@example.com';
+let password : string = 'abcabcabc';
+
+
+if (password == 'adfadfadf' || password == 'abcabcabc') {
+  console.log('Authorization granted!');
+} else {
+  console.log('Permission denied!');
+}
+
+
+ sounds like Unless!
+ 
+if(!(email == 'test@example.com')) {
+  console.log('You are authorized');
+} else {
+  console.log('Try again and again!');
+}
